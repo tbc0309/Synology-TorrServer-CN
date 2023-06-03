fd2584a4fa5f87c4b863db55c191a9a585964a76
我不是矿神 <39237997+tbc0309@users.noreply.github.com>
2023-06-03 13:25:13 +0800

    Update AddDialog.jsx

--- web/src/components/Add/AddDialog.jsx
+++ web/src/components/Add/AddDialog.jsx
@@ -41,7 +41,7 @@ export default function AddDialog({
   const [isUserInteractedWithPoster, setIsUserInteractedWithPoster] = useState(isEditMode)
   const [currentLang] = useChangeLanguage()
   const [selectedFile, setSelectedFile] = useState()
-  const [posterSearchLanguage, setPosterSearchLanguage] = useState(currentLang === 'ru' ? 'ru' : 'en')
+  const [posterSearchLanguage, setPosterSearchLanguage] = useState(currentLang === 'cn' ? 'cn' : 'en')
   const [isSaving, setIsSaving] = useState(false)
   const [skipDebounce, setSkipDebounce] = useState(false)
   const [isCustomTitleEnabled, setIsCustomTitleEnabled] = useState(false)
