0e58d88c23ea6bb9e34eb51f845afc6ec3a27596
我不是矿神 <39237997+tbc0309@users.noreply.github.com>
2023-10-11 22:05:57 +0800

    ZH

--- web/src/i18n.js
+++ web/src/i18n.js
@@ -1,23 +1,23 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
+import translationZH from 'locales/zh/translation.json'
 import translationEN from 'locales/en/translation.json'
 import translationRU from 'locales/ru/translation.json'
 import translationUA from 'locales/ua/translation.json'
-import translationZH from 'locales/zh/translation.json'
 import translationBG from 'locales/bg/translation.json'
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en', // default language will be used if none of declared lanuages detected (en, ru)
+    fallbackLng: 'zh', // default language will be used if none of declared lanuages detected (en, ru)
     interpolation: { escapeValue: false }, // react already safes from xss
     resources: {
+      zh: { translation: translationZH },
       en: { translation: translationEN },
       ru: { translation: translationRU },
       ua: { translation: translationUA },
-      zh: { translation: translationZH },
       bg: { translation: translationBG },
     },
   })
