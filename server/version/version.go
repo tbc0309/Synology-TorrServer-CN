d266990face0a530880a19a3e39666d21931aed9
YouROK <8yourok8@mail.ru>
2026-02-25 17:21:34 +0300

    MatriX.141

--- server/version/version.go
+++ server/version/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Version is set at build time via -ldflags "-X server/version.Version=<tag>"
-var Version = "MatriX.140"
+var Version = "MatriX.141"
 
 func GetTorrentVersion() string {
 	bi, ok := debug.ReadBuildInfo()
